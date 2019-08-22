6e5a8682e5763c28b6b27a628598e383b200332a
Kartikei Mittal <kartikeimittal@gmail.com>
2019-08-22 12:26:57 +0530

    Added bresenham algo

---
+++ Lab-2_22-08-2019/bresenham.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#include <graphics.h>
+using namespace std;
+
+void my_draw_line(int x1, int y1, int x2, int y2)
+{
+    // if(x1 > x2) {
+    //     swap(x1,x2); swap(y1,y2);
+    // }
+    int sign = (x1>x2)? -1 : 1;
+    int dx = x2-x1, dy = y2-y1;
+    int pK = 2*(dy-dx);
+    for(int x=x1, y=y1; x < x2; x += sign)
+    {
+        putpixel(x, y, WHITE);
+        if(pK < 0)
+            pK += 2*dy;
+        else
+        {
+            ++y;
+            pK += 2*(dy-dx);
+        }
+    }
+}
+
+int main()
+{
+    // line(2, 10, 8, 17);
+    // my_draw_line(2, 10, 40, 50);
+    int x1,x2,y1,y2;
+    cout<<"Enter points: ";
+    cin>>x1>>y1>>x2>>y2;
+    initwindow(400, 300, "Graphics Window");
+    my_draw_line(x1, y1, x2, y2);
+    while(!kbhit())
+        delay(100);
+    return 0;
+}
